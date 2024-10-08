ff17bfc42bc327b9063b92d86d923dce5c891006
MatejCervenka <127132225+MatejCervenka@users.noreply.github.com>
2024-10-08 11:41:17 +0200

    add: webhook

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const mysql = require('mysql2');
 const path = require('path');
+const app = express();
 const crypto = require('crypto');
 const { exec } = require('child_process');
 const bodyParser = require('body-parser');
-const app = express();
-const port = 80;
 
-const secret = '0971';  // Set this later when you create the webhook on GitHub.
+const port = 3000;
+
+const secret = '0971';
+
 app.use(bodyParser.json());
 
 // Připojení k MySQL databázi
 const db = mysql.createConnection({
-    host: '13.60.255.249',
-    user: 'root',        // Použijte své uživatelské jméno MySQL
-    password: '',  // Heslo k MySQL
+    host: 'localhost',
+    user: 'ubuntu',        // Použijte své uživatelské jméno MySQL
+    password: 'emirates139',  // Heslo k MySQL
     database: 'blog'     // Název databáze
 });
 
@@ -32,12 +34,26 @@ app.use(express.static(path.join(__dirname)));
 
 // Route pro získání příspěvků z databáze
 app.get('/posts', (req, res) => {
+    console.log('Fetching posts...');
     const query = 'SELECT * FROM posts ORDER BY created_at DESC';
     db.query(query, (err, results) => {
         if (err) {
             return res.status(500).send(err);
         }
         res.json(results);
+    });
+});
+
+// Add new post
+app.post('/add_post', (req, res) => {
+    const { title, content } = req.body;
+    const sql = 'INSERT INTO posts (title, content, created_at) VALUES (?, ?, NOW())';
+    db.query(sql, [title, content], (err) => {
+        if (err) {
+            res.status(500).send('Error inserting post');
+        } else {
+            res.status(200).send('Post added');
+        }
     });
 });
 
